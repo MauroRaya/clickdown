2cda4536dfc053e70d40f898619b92770b3bf998
MauroRaya <maurorayabr@gmail.com>
2025-02-14 09:59:12 -0300

    feat: add models

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using clickdown.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace clickdown.Data;
+
+public class AppDbContext : DbContext {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+    
+    public DbSet<User> Users { get; set; }
+    public DbSet<Job> Jobs { get; set; }
+}
---
+++ Models/Job.cs
@@ -0,0 +1,8 @@
+namespace clickdown.Models;
+
+public class Job {
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Body { get; set; } = string.Empty;
+    public int UserId { get; set; }
+}
